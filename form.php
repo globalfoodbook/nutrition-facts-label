608af2e2f04d55c2142406852ead6bc09653ae33
Ikenna Okpala <me@ikennaokpala.com>
2015-10-21 18:55:21 +0100

    Change heading and settings tags

--- form.php
+++ form.php
@@ -1,3 +1,3 @@
 <div class='wrap'>
-<h2><?php echo __( 'Nutrition Facts Label', 'menu-nutrition-label' ) ?> </h2>
+<h2><?php echo __( 'Nutrition Facts Label by <a href="http://globalfoodbook.com" target="_blank">Global Food Book</a>', 'menu-gfb-nutrition-label' ) ?> </h2>
 </div>

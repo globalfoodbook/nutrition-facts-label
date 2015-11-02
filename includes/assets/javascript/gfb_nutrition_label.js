b207d7a2727e7911558ad1d5bc6384349c69b8de
Ikenna Okpala <me@ikennaokpala.com>
2015-11-02 03:18:24 +0000

    Background settings for nutrition label

--- includes/assets/javascript/gfb_nutrition_label.js
+++ includes/assets/javascript/gfb_nutrition_label.js
@@ -3,9 +3,16 @@ var GFBNutritionLabel = function(){};
 GFBNutritionLabel.prototype.generateImage = function(){
   var node = [document.getElementById('nutrition-label')];
   html2canvas(node, {
-   // logging: true,
-   // useCORS: true,
+    // logging: true,
+    useCORS: true,
+    background :'#FFFFFF',
+    allowTaint: true,
+    letterRendering: true,
     onrendered: function(canvas) {
+      var context = canvas.getContext('2d');
+      context.webkitImageSmoothingEnabled = false;
+      context.mozImageSmoothingEnabled = false;
+      context.imageSmoothingEnabled = false;
 			var image = canvas.toDataURL("image/jpeg");
 			// console.log("Image: ", image);
 			// window.location.href=image;

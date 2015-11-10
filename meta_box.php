6d5ded3553f746d6db0b2470f7ac1f6d010c7e1b
Ikenna Okpala <me@ikennaokpala.com>
2015-11-10 22:13:50 +0000

    Add beta image to meta box

--- meta_box.php
+++ meta_box.php
@@ -2,7 +2,8 @@
 add_action( 'add_meta_boxes', 'add_nutrition_label_meta_box' );
 
 function add_nutrition_label_meta_box() {
-  add_meta_box( 'gfb-meta-box-', 'Nutrition Facts Label Generator', 'gfb_nutrition_label_meta_box', 'recipe', 'normal', 'high' );
+  $title = "Nutrition Facts Label Generator ". beta_img();
+  add_meta_box( 'gfb-meta-box-', $title, 'gfb_nutrition_label_meta_box', 'recipe', 'normal', 'high' );
 }
 function gfb_nutrition_label_meta_box(){?>
   <style>

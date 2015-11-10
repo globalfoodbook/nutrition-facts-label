6615756d99762cabceee42a9ed0c73736651fa8f
Ikenna Okpala <me@ikennaokpala.com>
2015-11-10 03:08:54 +0000

    Add styling

--- meta_box.php
+++ meta_box.php
@@ -4,9 +4,15 @@ add_action( 'add_meta_boxes', 'add_nutrition_label_meta_box' );
 function add_nutrition_label_meta_box() {
   add_meta_box( 'gfb-meta-box-', 'Nutrition Facts Label Generator', 'gfb_nutrition_label_meta_box', 'recipe', 'normal', 'high' );
 }
-function gfb_nutrition_label_meta_box(){
-  gfb_nutrition_label_add_to_head();
-  require_once 'form.php';
+function gfb_nutrition_label_meta_box(){?>
+  <style>
+    .gfb-nutrition-label-section {
+      padding-top: 0!important;
+    }
+  </style>
+  <?php
+    $hidden = "style='display:none'";
+    gfb_nutrition_label_add_to_head();
+    require_once 'form.php';
 }
-
 ?>

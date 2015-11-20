7bc5fa2c8e941d45edeb9199ba05536ff9968815
Ikenna Okpala <me@ikennaokpala.com>
2015-11-20 02:00:03 +0000

    Add notification tag

--- form.php
+++ form.php
@@ -14,10 +14,16 @@
     display: inline-block !important;
     overflow: hidden;
   }
+  #gfb-nutrition-label-notify {
+    color: red;
+    font-weight: bold;
+    margin-left: 12px;
+  }
 </style>
 <div class='wrap'>
   <div class='gfb-nutrition-label-section'>
     <img id="gfb-nutrition-label-loader" src="<?php echo plugin_dir_url( __FILE__ ) .'includes/assets/images/load.gif'?>" style="display:none"/>
+    <p id="gfb-nutrition-label-notify" style="display:none"></p>
     <div>
       <?php require_once 'sections.php'; ?>
     </div>

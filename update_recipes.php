93624a54ddd7e9386badf9a92a1755d37eed653c
Ikenna Okpala <me@ikennaokpala.com>
2015-11-22 17:04:31 +0000

    Update all posts with nutrition facts data

--- update_recipes.php
+++ update_recipes.php
@@ -14,7 +14,34 @@
   }
 </style>
 <div class='wrap'>
+  <p>NB: You need to be an administrator to use this feature. </p>
   <div class='gfb-nutrition-label-section' <?php echo "test"; ?> >
-    <input id="gfb-nutrition-label-update-button" type='button' name='Submit' value='Update All Recipes' style="" onclick="gfbnutritionlabel.update()"/>
+    <form name='gfb-nutrition-label-form' method='post' action='#'>
+      <input id='gfb-nutrition-label-update-button' type='submit' name='Submit' value='Update All Recipes' style='' />
+    </form>
   </div>
 </div>
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (is_admin()) {
+      $args=array(
+        'post_type' => 'recipe',
+        'post_status' => 'publish'
+      );
+      $my_query = new WP_Query($args);
+      if( $my_query->have_posts() ) {
+        while ($my_query->have_posts()) : $my_query->the_post();
+        $post_id = get_the_ID();
+        $ingredients = get_post_meta($post_id, 'RECIPE_META_ingredients', true);
+        $nutrition_facts =  process_request($ingredients);
+        if (!add_post_meta($post_id, META_KEY, $nutrition_facts, true)) {
+           update_post_meta ($post_id, META_KEY, $nutrition_facts);
+        } ?>
+          <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Update complete <?php the_title_attribute(); ?>"><?php the_title(); ?></a> Update Complete!</p>
+          <?php
+        endwhile;
+      }
+      wp_reset_query();
+    }
+  }
+?>

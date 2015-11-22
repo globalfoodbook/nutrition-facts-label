ad43b66dfdf9e2f08bc3e37210714d6e71be69e6
Ikenna Okpala <me@ikennaokpala.com>
2015-11-22 01:51:38 +0000

    Add update recipes

---
+++ update_recipes.php
@@ -0,0 +1,20 @@
+<style media="screen">
+  #gfb-nutrition-label-update-button {
+    display: block;
+    width: 140px;
+    background: #4E9CAF;
+    padding: 10px;
+    text-align: center;
+    border-color:transparent;
+    border-radius: 5px;
+    color: white;
+    font-size:13.5px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+</style>
+<div class='wrap'>
+  <div class='gfb-nutrition-label-section' <?php echo "test"; ?> >
+    <input id="gfb-nutrition-label-update-button" type='button' name='Submit' value='Update All Recipes' style="" onclick="gfbnutritionlabel.update()"/>
+  </div>
+</div>

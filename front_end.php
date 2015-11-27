13a61eefc3aae0d92385db3a07b0460f85ff1ef8
kengimel <kengimel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 22:26:11 +0000

    Avoid autofill when response is not available
    git-svn-id: http://plugins.svn.wordpress.org/nutrition-facts-label/trunk@1296102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- front_end.php
+++ front_end.php
@@ -14,9 +14,12 @@ jQuery( document ).ready(function() {
     jQuery('#nutrition-label').nutritionLabel(jQuery.extend( settings, response ));
 
   <?php } else if (!empty($post) && $post->post_type == 'recipe') { ?>
-    var response = <?php echo json_encode(get_post_meta($post->ID, META_KEY)); ?>;
-
-    jQuery('#nutrition-label').nutritionLabel(jQuery.extend( settings, response ));
+    var response = <?php echo json_encode(get_post_meta($post->ID, META_KEY, true)); ?>;
+    if (response.length > 0){
+      jQuery('#nutrition-label').nutritionLabel(jQuery.extend( settings, response ));
+    } else {
+      jQuery('#nutrition-label').nutritionLabel()
+    }
 
   <?php } else { ?>
     jQuery('#nutrition-label').nutritionLabel()

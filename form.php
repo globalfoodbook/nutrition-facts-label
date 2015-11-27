8262c0f1a3ec3250f5c2b6aba38951a1c141a8e9
kengimel <kengimel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 22:03:35 +0000

    enqueue dependencies and modify layout
    git-svn-id: http://plugins.svn.wordpress.org/nutrition-facts-label/trunk@1296045 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,6 +1,18 @@
 <style media="screen">
+  .gfb-nutrition-label-section{
+    padding-top: 30px;
+  }
+  #nutrition-label-outer {
+    background-color: white;
+    width: auto !important;
+  }
+  .gfb-nutrition-label-section .section2 {
+    padding-left: 40px
+  }
   .gfb-nutrition-label-section .section1, .gfb-nutrition-label-section .section2 {
-    width:350px !important;
+    vertical-align: top;
+    display: inline-block !important;
+    overflow: hidden;
   }
 </style>
 <div class='wrap'>
@@ -14,6 +26,14 @@
       </form>
     </div>
     <div class='section2'>
+      <div id="nutrition-label-outer">
+        <div id="nutrition-label"> </div>
+      </div>
     </div>
   </div>
 </div>
+<script type="text/javascript">
+jQuery( document ).ready(function() {
+  jQuery('#nutrition-label').nutritionLabel();
+});
+</script>

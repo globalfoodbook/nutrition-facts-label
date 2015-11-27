9b29011064eb81207496120a3e9503949df72ead
kengimel <kengimel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 22:03:13 +0000

    Formulate sections
    git-svn-id: http://plugins.svn.wordpress.org/nutrition-facts-label/trunk@1296044 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,8 +1,19 @@
+<style media="screen">
+  .gfb-nutrition-label-section .section1, .gfb-nutrition-label-section .section2 {
+    width:350px !important;
+  }
+</style>
 <div class='wrap'>
   <h1><?php echo __( 'Nutrition Facts Label by <a href="http://globalfoodbook.com" target="_blank">Global Food Book</a>', 'menu-gfb-nutrition-label' ) ?> </h1>
-  <form method='post' action='form.php'>
-     <h2>Ingredients:</h2>
-     <p><textarea name='ingredients' rows='12' cols='33'></textarea></p>
-     <p><input type='submit' name='Submit' value='Generate Label' /></p>
-  </form>
+  <div class='gfb-nutrition-label-section'>
+    <div class='section1'>
+      <form method='post' action='form.php'>
+         <h4>Ingredients:</h4>
+         <p><textarea name='ingredients' rows='12' cols='33'></textarea></p>
+         <p><input type='submit' name='Submit' value='Generate Label' /></p>
+      </form>
+    </div>
+    <div class='section2'>
+    </div>
+  </div>
 </div>

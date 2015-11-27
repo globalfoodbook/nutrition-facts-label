105c0b48935786cfb622fd179eaea675bfc8c9da
kengimel <kengimel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 22:02:49 +0000

    Add textarea
    git-svn-id: http://plugins.svn.wordpress.org/nutrition-facts-label/trunk@1296043 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,3 +1,8 @@
 <div class='wrap'>
-<h2><?php echo __( 'Nutrition Facts Label by <a href="http://globalfoodbook.com" target="_blank">Global Food Book</a>', 'menu-gfb-nutrition-label' ) ?> </h2>
+  <h1><?php echo __( 'Nutrition Facts Label by <a href="http://globalfoodbook.com" target="_blank">Global Food Book</a>', 'menu-gfb-nutrition-label' ) ?> </h1>
+  <form method='post' action='form.php'>
+     <h2>Ingredients:</h2>
+     <p><textarea name='ingredients' rows='12' cols='33'></textarea></p>
+     <p><input type='submit' name='Submit' value='Generate Label' /></p>
+  </form>
 </div>

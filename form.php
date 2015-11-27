eef2dc9811e22c3222abb5e8b9d8c1037d1a59e7
kengimel <kengimel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-27 22:05:01 +0000

    Process request
    git-svn-id: http://plugins.svn.wordpress.org/nutrition-facts-label/trunk@1296048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- form.php
+++ form.php
@@ -1,3 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (!empty($_POST["ingredients"])) {
+    $ingredients = $_POST["ingredients"];
+    process_request($ingredients);
+  }
+}
+
+function process_request($ingredients){
+  $api = "http://nutsv.globalfoodbook.net/v1/nutrition/facts?ingredients=";
+  $url = $api.urlencode(implode(",", explode("\n", trim($ingredients))));
+  $opts = array(
+  'http'=>array(
+    'method'=>"GET"
+    )
+  );
+
+  $context = stream_context_create($opts);
+  $json = file_get_contents($url, 0, $context);
+  $response = json_decode($json);
+  return $response;
+}
+?>
 <style media="screen">
   .gfb-nutrition-label-section{
     padding-top: 30px;
@@ -19,9 +42,9 @@
   <h1><?php echo __( 'Nutrition Facts Label by <a href="http://globalfoodbook.com" target="_blank">Global Food Book</a>', 'menu-gfb-nutrition-label' ) ?> </h1>
   <div class='gfb-nutrition-label-section'>
     <div class='section1'>
-      <form method='post' action='form.php'>
+      <form method='post' action='<?php echo admin_url( 'admin.php' ); ?>?page=gfb-nutrition-label-settings'>
          <h4>Ingredients:</h4>
-         <p><textarea name='ingredients' rows='12' cols='33'></textarea></p>
+         <p><textarea name='ingredients' rows='12' cols='33'><?php echo $ingredients; ?></textarea></p>
          <p><input type='submit' name='Submit' value='Generate Label' /></p>
       </form>
     </div>

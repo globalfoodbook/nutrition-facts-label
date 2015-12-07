ffd5aa4f45ede83b3be7f4513718d95fb2352df6
Ikenna Okpala <me@ikennaokpala.com>
2015-12-07 02:07:50 +0000

    Cosmetic changes

--- front_end.php
+++ front_end.php
@@ -1,12 +1,11 @@
 <?php $serving = get_post_meta($post->ID, 'RECIPE_META_servings', true);?>
 <script type="text/javascript">
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     <?php if (!empty($post) && $post->post_type == 'recipe') { ?>
     var response = <?php echo json_encode(get_post_meta($post->ID, META_KEY, true)); ?>;
     if (response.length > 0){
       gfbnutritionlabel.setup(response, <?php echo json_encode(options($ingredients)); ?>, <?php echo (empty($serving) ? 1 : $serving)?>)
     }
-
-  <?php }  ?>
-});
+    <?php }  ?>
+  });
 </script>
